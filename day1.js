207a2bb47d4118b308163438293e66f384c40de2
Muralidharan Vivekananthan <87437063+muralitherun@users.noreply.github.com>
2023-08-04 11:46:31 +0530

    CreateD day1.js

---
+++ day1.js
@@ -0,0 +1,30 @@
+var petDog = 'Rex';
+var petCat = 'Pepper';
+
+console.log(petDog);
+console.log(petCat);
+
+console.log("My pet dog's name is:", petDog);
+console.log("My pet cat's name is:", petCat);
+
+var dogSound = 'woof';
+var catSound = 'purr';
+
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+
+var catSound = 'meow';
+console.log(petCat, "now says", catSound);
+
+/*
+OUTPUT:
+
+Rex
+Pepper
+My pet dog's name is: Rex
+My pet cat's name is: Pepper
+Rex says woof
+Pepper says purr
+Pepper now says meow
+
+*/
